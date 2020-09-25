6b248d352900b697df58f48ce4168826d5c78be8
Mariano <mavico@ignetworks.com>
2020-09-25 12:33:04 -0300

    paro con este curso para darle a angular y a django

--- VeterinariaBack/index.js
+++ VeterinariaBack/index.js
@@ -1,1 +1,34 @@
-console.log('Hola mundo')
+const http = require('http');
+const url = require('url');
+
+const callbackDelServidor = (req, res) => {
+    //1.obtener url desde objeto request //OK
+    const urlActual = req.url;
+    const urlParseada = url.parse(urlActual, true);
+
+    //2.obtener la ruta
+    const ruta = urlParseada.pathname;
+
+    //3. quitar slash a la ruta
+    const rutaLimpia = ruta.replace('/',"")
+    console.log(rutaLimpia);
+
+    //3.1 obtener el método http
+    console.log('req.method', req.method);
+
+    //4. enviar una respuet dpendiendo de la ruta
+    if (rutaLimpia === 'ruta') {
+        res.end('Hola estas en /ruta');
+    } else {
+        res.end('estas en una ruta que no conozco');
+    }
+
+
+    res.end('Hola mundo en un server http');
+}
+
+const server = http.createServer(callbackDelServidor);
+
+server.listen(5000, () => {
+    console.log('el servidor está escuchando peticiones en http://localhost:5000/');
+});
